a5bf0093ce174ba00ba40c614b1db14531fbbf4b
Bryan Pan <bryanpan342@gmail.com>
2021-03-09 12:04:56 -0800

    chore: adding some documentation, linter, and mergify configuration

--- src/index.js
+++ src/index.js
@@ -4,27 +4,30 @@
  *  - Subtle Patterns
  */
 
+// eslint-disable-next-line no-undef
+const _PHASER = Phaser;
+
 /**
  * PHASER
  */
 let config = {
-  type: Phaser.AUTO,
+  type: _PHASER.AUTO,
   width: window.innerWidth,
   height: window.innerHeight,
-  backgroundColor: "#222222",
-  parent: "game-container",
+  backgroundColor: '#222222',
+  parent: 'game-container',
   scene: {
     preload: preload,
     create: create,
-    update: update
-  }
+    update: update,
+  },
 };
 
-const game = new Phaser.Game(config);
+const game = new _PHASER.Game(config);
 
 function preload() {
-  // "this" === Phaser.Scene
-  this.load.image("repeating-background", "../assets/preload.png");
+  // 'this' === Phaser.Scene
+  this.load.image('repeating-background', '../assets/preload.png');
 }
 
 function create() {
@@ -32,20 +35,20 @@ function create() {
   const { width, height } = this.sys.game.config;
 
   // Creating a repeating background sprite
-  const bg = this.add.tileSprite(0, 0, width, height, "repeating-background");
+  const bg = this.add.tileSprite(0, 0, width, height, 'repeating-background');
   bg.setOrigin(0, 0);
 
-  // In v3, you can chain many methods, so you can create text and configure it in one "line"
+  // In v3, you can chain many methods, so you can create text and configure it in one 'line'
   this.add
-    .text(width / 2, height / 2, "WELCOME\nTO\nPOMPEII", {
-      font: "175px monospace",
-      color: "white"
+    .text(width / 2, height / 2, 'WELCOME\nTO\nPOMPEII', {
+      font: '175px monospace',
+      color: 'white',
     })
     .setOrigin(0.5, 0.5)
-    .setShadow(5, 5, "#5588EE", 0, true, true);
+    .setShadow(5, 5, '#5588EE', 0, true, true);
 }
 
-function update(time, delta) {
+function update(_time, _delta) {
   // Here is where you can run logic that you need to check over time
   // e.g. updating a player sprite's position based on keyboard input
 }
